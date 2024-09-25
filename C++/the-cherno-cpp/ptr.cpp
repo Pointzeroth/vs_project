c0697650347c2c8d6aa1dde7711be60a17749058
Yldlrs <point316@foxmail.com>
2024-09-25 20:17:13 +0800

    cpp的指针

---
+++ C++/the-cherno-cpp/ptr.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#define LOG(x) std::cout << x << std::endl
+
+int main()
+{
+    int var = 8;
+    void* ptr = nullptr;       //定义了一个无类型的指针，它的内存地址是0    NULL/nullptr，不能直接赋值0，0地址是无意义的。
+    std::cin.get();
+}
